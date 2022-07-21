a979723ed06add863f7b5d09d3deec4d80fe74e8
Suwichan <suwichan@mmc.co.jp>
2022-07-21 09:52:35 +0700

    New project

---
+++ HashTableApp/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HashTableApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Hashtable hashtable;
+        public MainWindow()
+        {
+            InitializeComponent();
+            hashtable = new Hashtable();
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //Add
+            hashtable.Add(keytxt.Text, valtxt.Text);
+        }
+
+        private void RemoveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //Remove
+            hashtable.Remove(keytxt.Text);
+        }
+
+        private void ShowBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ICollection icollection = hashtable.Keys;
+            //Show
+            foreach(string key in icollection)
+            {
+                MessageBox.Show(hashtable[key].ToString());
+            }
+        }
+    }
+}
